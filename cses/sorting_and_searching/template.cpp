b7729265fd9a05aeb96d0e7aa70397ae0de14baf
hrittik16 <hrittikdas16@gmail.com>
2020-06-14 23:52:43 +0530

    updated

---
+++ cses/sorting_and_searching/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define vi vector<int>
+#define vl vector<ll>
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repll(i, a, b) for(ll i = a; i < b; i++)
+
+int main() {
+
+	
+
+
+
+
+	return 0;
+}

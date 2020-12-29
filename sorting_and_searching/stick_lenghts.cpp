62fdc5d6b2a4666c1c5f7322560906cfb6aa605d
hrittik16 <hrittikdas16@gmail.com>
2020-12-29 14:58:44 +0530

    Updated

---
+++ sorting_and_searching/stick_lenghts.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int 				long long
+#define f 					first
+#define s 					second
+#define pb 					push_back
+#define mp 					make_pair
+#define vi 					vector<int>
+#define pii					pair<int, int>
+#define mii					map<int, int>
+#define all(a) 				(a).begin(),(a).end()
+#define sz(x) 				(x).size()
+#define rep(i, a, b) 		for(int i = a; i < b; i++)
+#define pqb					priority_queue<int>
+#define pqs					priority_queue<int, vi, greater<int>>
+#define setbits(x)			__builtin_popcountll(x)
+#define zerobits(x)			__builtin_ctzll(x) // counts number of leading 0's before 1st occurnace of 1
+#define mod 				1000000007
+#define inf					1e18+7
+#define ps(x, y)			fixed << setprecision(y) << x
+#define string_to_int(x)	stoi(x)
+#define int_to_string(x)	to_string(x)
+#define test				int T; cin >> T; while(T--)
+
+void start() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main() {
+	start();
+
+	int n;
+	cin >> n;
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+	sort(all(a));
+	unordered_map<int, int> fre;
+
+	for (auto x : a) fre[x]++;
+
+	int mx = 0, num = 0;
+
+	for (auto x : fre) {
+		if (x.second > mx) {
+			mx = x.second;
+			num = x.first;
+		}
+	}
+	int total1 = 0, total2 = 0;
+	for (auto x : a) total1 += abs(x - num);
+
+	int mid = n / 2;
+
+	rep(i, 0, n) total2 += abs(a[i] - a[mid]);
+
+	cout << min(total1, total2) << "\n";
+
+	return 0;
+}

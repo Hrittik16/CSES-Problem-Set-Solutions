65e24d482e62802d29631b4e8fb678e808efa2b9
Hrittik16 <hrittikdas16@gmail.com>
2021-08-04 23:00:23 +0530

    updated

--- sorting_and_searching/concert_tickets.cpp
+++ sorting_and_searching/concert_tickets.cpp
@@ -6,48 +6,29 @@ using namespace std;
 #define pii pair<int, int>
 #define mp make_pair
 #define rep(i, a, b) for(int i = a; i < b; i++)
-#define all(x) (x)->begin(), (x)->end()
+#define all(x) (x).begin(), (x).end()
 #define f first
 #define s second
 
 int32_t main() {
 	int n, m;
 	cin >> n >> m;
-	vi a(n), b(m);
-	map<int, int> mp;
+	vi a(n);
+	set<pii> st;
 	rep(i, 0, n) {
 		cin >> a[i];
-		mp[a[i]]++;
+		st.insert(mp(a[i], i));
 	}
-	rep(i, 0, m) cin >> b[i];
-	rep(j, 0, m) {
-		auto it = mp.lower_bound(b[j]);
-		if(it->f == b[j]) {
-			it->s--;
-			cout << it->f << "\n";
-			if(it->s == 0) mp.erase(it);
-		} 
-		else if(it == mp.begin()) {
-			if(it->f > b[j]) cout << "-1\n";
-			else {
-				cout << it->f << "\n";
-				it->s--;
-				if(it->s == 0) mp.erase(it);
-			}
-		}
+	
+	rep(i, 0, m) {
+		int x;
+		cin >> x;
+		auto it = st.lower_bound(mp(x+1, 0));
+		if(it == st.begin()) cout << "-1\n";
 		else {
 			it--;
-			if(it->f > b[j]) cout << "-1\n";
-			else {
-				cout << it->f << "\n";
-				it->s--;
-				if(it->s == 0) mp.erase(it);
-			}	
-		}
-
-		if(mp.empty()) {
-			rep(k, 0, m-j-1) cout << "-1\n";
-			break;
+			cout << it->f << "\n";
+			st.erase(it);
 		}
 	}
 }

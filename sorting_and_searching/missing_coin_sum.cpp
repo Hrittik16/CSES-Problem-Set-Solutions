48cccb6d08c07bd8c0608ab0c3125084ab2052ac
Hrittik16 <hrittikdas16@gmail.com>
2021-08-06 11:56:08 +0530

    Updated

---
+++ sorting_and_searching/missing_coin_sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define vi vector<int>
+#define pii pair<int, int>
+#define mp make_pair
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define pb push_back
+
+int32_t main() {
+	int n;
+	cin >> n;
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+	sort(all(a));
+	
+}
